61ae5b0cbf31ec3f72bacd09744d2acf3419e2c4
Simon Kurz <Simon.Kurz@desy.de>
2017-05-23 16:23:50 +0200

    ported and fixed muonBremsstrahlung

--- FastSimulation/SimplifiedGeometryPropagator/interface/Constants.h
+++ FastSimulation/SimplifiedGeometryPropagator/interface/Constants.h
@@ -7,7 +7,9 @@ namespace fastsim
     {
 		static double constexpr speedOfLight = 29.9792458;  // [cm / ns]
 		static double constexpr eMass = 0.0005109990615;
-		static constexpr double epsilonDistance_ =  1.0e-7; // [cm]
+		static double constexpr muMass = 0.1056583745;
+		static double constexpr epsilonDistance_ =  1.0e-7; // [cm]
+		static double constexpr NA = 6.022e+23;
     };
 }
 
